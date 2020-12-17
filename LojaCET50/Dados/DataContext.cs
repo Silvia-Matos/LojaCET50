aefcc7c2e407430f326bcc60707b3f6c5bbde203
Silvia Matos <Silvia.Duarte.Matos@formandos.cinel.pt>
2020-12-17 18:12:10 +0000

    Criar a base de dados e tabela produtos.

---
+++ LojaCET50/Dados/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using LojaCET50.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace LojaCET50.Dados
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
